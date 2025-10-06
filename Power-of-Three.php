11e011a35ff74d0f47365e862f33c7a24d87074e
chrismach-gos <chris.mach.gos@gmail.com>
2025-10-06 17:05:55 +0700

    Create Power-of-Three.php

---
+++ Power-of-Three.php
@@ -0,0 +1,16 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $n
+     * @return Boolean
+     */
+    function isPowerOfThree($n) {
+        if($n < 1) return false;
+        while($n > 1) {
+            if($n % 3 != 0) return false;
+            $n /= 3;
+        }
+        return true;
+    }
+}
