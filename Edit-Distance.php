488a8a0ed5f2310fa30e7f949e0ba637b2c7c92b
chrismach-gos <chris.mach.gos@gmail.com>
2025-10-06 17:11:21 +0700

    Create Edit-Distance.php

---
+++ Edit-Distance.php
@@ -0,0 +1,32 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $word1
+     * @param String $word2
+     * @return Integer
+     */
+    function minDistance($word1, $word2) {
+        $l1 = strlen($word1);
+        $l2 = strlen($word2);
+
+        $dp = [];
+
+        for($i = 0; $i <= $l1; ++$i)
+            $dp[$i][0] = $i;
+        
+        for($i = 0; $i <= $l2; ++$i)
+            $dp[0][$i] = $i;
+        
+        for($i = 1;$i <= $l1; ++$i){
+            for($j = 1; $j <= $l2; ++$j){
+                if($word1[$i-1] === $word2[$j-1]) 
+                    $dp[$i][$j] = $dp[$i-1][$j-1];
+                else
+                    $dp[$i][$j] = 1 + min($dp[$i][$j-1], $dp[$i-1][$j], $dp[$i-1][$j-1]);
+            }
+        }
+
+        return $dp[$l1][$l2];
+    }
+}
